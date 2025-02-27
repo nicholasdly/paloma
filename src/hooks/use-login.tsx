4cf96f631ffbc853e9e25c8f5f727359768c9013
Nicholas Ly <nichdly@gmail.com>
2025-02-27 00:09:19 -0600

    feat: delete chat endpoint + fetch abstractions

--- src/hooks/use-login.tsx
+++ src/hooks/use-login.tsx
@@ -2,37 +2,8 @@ import { useRouter } from "next/navigation";
 import { useTransition } from "react";
 import { toast } from "sonner";
 import { z } from "zod";
+import { login } from "@/lib/api";
 import { loginFormSchema } from "@/lib/auth/schemas";
-import { tc } from "@/lib/utils";
-
-export async function login(values: z.infer<typeof loginFormSchema>) {
-  const [response] = await tc(
-    fetch("/api/login", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(values),
-    }),
-  );
-
-  if (response?.ok) {
-    return {
-      success: true as const,
-      error: null,
-    };
-  }
-
-  if (response?.status === 401) {
-    return {
-      success: false as const,
-      error: "Incorrect email or password.",
-    };
-  }
-
-  return {
-    success: false as const,
-    error: "Something went wrong! Try again later.",
-  };
-}
 
 export function useLogin() {
   const [isPending, startTransition] = useTransition();
